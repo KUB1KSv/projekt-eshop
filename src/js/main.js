1288de87ec615f9be8b87a96f8c9900b98cd72ec
KUB1KSv <kuubbaassvv@gmail.com>
2024-12-08 13:49:50 +0100

    5

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 // Import our custom CSS
 import '../scss/styles.scss'
 
+
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+  });
+
 // Import all of Bootstrap's JS
 import {Dropdown} from 'bootstrap';
 
